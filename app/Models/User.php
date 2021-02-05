b3f754024a924b7425980718a1148086f90ffe7c
William <william@websnack.dk>
2021-02-05 23:06:48 +0100

    Added: basic laravel setup
    Added: laravel breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
